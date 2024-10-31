ee3c9f42474bd55ef72029954e27e786204206ca
Ian <idelacruz@solintels.com>
2024-10-30 23:42:39 -0500

    No Return Null

--- src/main/java/com/example/actividadruc/service/impl/PersonaJuridicaServiceImpl.java
+++ src/main/java/com/example/actividadruc/service/impl/PersonaJuridicaServiceImpl.java
@@ -8,7 +8,9 @@ import com.example.actividadruc.repository.PersonaJuridicaRepository;
 import com.example.actividadruc.service.PersonaJuridicaService;
 import feign.FeignException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.sql.Timestamp;
 import java.util.NoSuchElementException;
@@ -86,11 +88,23 @@ public class PersonaJuridicaServiceImpl implements PersonaJuridicaService {
 
 
     private PersonaJuridicaEntity getEntity(String ruc){
-        PersonaJuridicaEntity personaJuridicaEntity = new PersonaJuridicaEntity();
-        ResponseSunat datosreniecj = executionSunatJ(ruc);
-        if(Objects.nonNull(datosreniecj)){
+
+        try {
+            ResponseSunat datosreniecj = executionSunatJ(ruc);
+
+            // Verifica si los datos recibidos son válidos
+            if (datosreniecj == null || datosreniecj.getRazonSocial() == null) {
+                return null;
+            }
+
+            PersonaJuridicaEntity personaJuridicaEntity = new PersonaJuridicaEntity();
             personaJuridicaEntity.setRazonSocial(datosreniecj.getRazonSocial());
             personaJuridicaEntity.setTipoDocumento(datosreniecj.getTipoDocumento());
+            // Configura los demás campos de la entidad...
+            personaJuridicaEntity.setUserCreated(Constants.USER_CREATED);
+            personaJuridicaEntity.setDateCreated(new Timestamp(System.currentTimeMillis()));
+            personaJuridicaEntity.setUserUpdate(Constants.USER_CREATED);
+            personaJuridicaEntity.setDateUpdate(new Timestamp(System.currentTimeMillis()));
             personaJuridicaEntity.setNumeroDocumento(datosreniecj.getNumeroDocumento());
             personaJuridicaEntity.setEstado(datosreniecj.getEstado());
             personaJuridicaEntity.setCondicion(datosreniecj.getCondicion());
@@ -120,8 +134,54 @@ public class PersonaJuridicaServiceImpl implements PersonaJuridicaService {
             personaJuridicaEntity.setDateCreated(new Timestamp(System.currentTimeMillis()));
             personaJuridicaEntity.setUserUpdate(Constants.USER_CREATED);
             personaJuridicaEntity.setDateUpdate(new Timestamp(System.currentTimeMillis()));
+
+            return personaJuridicaEntity;
+
+        } catch (HttpClientErrorException e) {
+            if (e.getStatusCode() == HttpStatus.UNPROCESSABLE_ENTITY) {
+                throw new IllegalArgumentException("RUC no válido");
+            }
+            throw new RuntimeException("Error en la API externa");
         }
-        return personaJuridicaEntity;
+
+
+
+//        PersonaJuridicaEntity personaJuridicaEntity = new PersonaJuridicaEntity();
+//        ResponseSunat datosreniecj = executionSunatJ(ruc);
+//        if(Objects.nonNull(datosreniecj)){
+//            personaJuridicaEntity.setRazonSocial(datosreniecj.getRazonSocial());
+//            personaJuridicaEntity.setTipoDocumento(datosreniecj.getTipoDocumento());
+//            personaJuridicaEntity.setNumeroDocumento(datosreniecj.getNumeroDocumento());
+//            personaJuridicaEntity.setEstado(datosreniecj.getEstado());
+//            personaJuridicaEntity.setCondicion(datosreniecj.getCondicion());
+//            personaJuridicaEntity.setDireccion(datosreniecj.getDireccion());
+//            personaJuridicaEntity.setUbigeo(datosreniecj.getUbigeo());
+//            personaJuridicaEntity.setViaTipo(datosreniecj.getViaTipo());
+//            personaJuridicaEntity.setViaNombre(datosreniecj.getViaNombre());
+//            personaJuridicaEntity.setZonaCodigo(datosreniecj.getZonaCodigo());
+//            personaJuridicaEntity.setZonaTipo(datosreniecj.getZonaTipo());
+//            personaJuridicaEntity.setNumero(datosreniecj.getNumero());
+//            personaJuridicaEntity.setInterior(datosreniecj.getInterior());
+//            personaJuridicaEntity.setLote(datosreniecj.getLote());
+//            personaJuridicaEntity.setDpto(datosreniecj.getDpto());
+//            personaJuridicaEntity.setManzana(datosreniecj.getManzana());
+//            personaJuridicaEntity.setKilometro(datosreniecj.getKilometro());
+//            personaJuridicaEntity.setDistrito(datosreniecj.getDistrito());
+//            personaJuridicaEntity.setProvincia(datosreniecj.getProvincia());
+//            personaJuridicaEntity.setDepartamento(datosreniecj.getDepartamento());
+//            personaJuridicaEntity.setEsAgenteRetencion(datosreniecj.isEsAgenteRetencion());
+//            personaJuridicaEntity.setTipo(datosreniecj.getTipo());
+//            personaJuridicaEntity.setActividadEconomica(datosreniecj.getActividadEconomica());
+//            personaJuridicaEntity.setNumeroTrabajadores(datosreniecj.getNumeroTrabajadores());
+//            personaJuridicaEntity.setTipoFacturacion(datosreniecj.getTipoFacturacion());
+//            personaJuridicaEntity.setTipoContabilidad(datosreniecj.getTipoContabilidad());
+//            personaJuridicaEntity.setComercioExterior(datosreniecj.getComercioExterior());
+//            personaJuridicaEntity.setUserCreated(Constants.USER_CREATED);
+//            personaJuridicaEntity.setDateCreated(new Timestamp(System.currentTimeMillis()));
+//            personaJuridicaEntity.setUserUpdate(Constants.USER_CREATED);
+//            personaJuridicaEntity.setDateUpdate(new Timestamp(System.currentTimeMillis()));
+//        }
+//        return personaJuridicaEntity;
     }
 
     private ResponseSunat executionSunatJ(String ruc){
